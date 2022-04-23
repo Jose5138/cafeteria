85d8cecef781da49c52ed16a7fd1aa0c978932b6
Jose Perez <cientifico141@gmail.com>
2022-04-23 19:20:50 +0100

    mi firts commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict"
+const btn_registro = document.getElementById('btn_registro');
+
+btn_registro.addEventListener('click', (e)=>{
+      e.preventDefault();
+      alert("este botón está desactivado");
+});
+
+
+
